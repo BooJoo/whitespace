fa76391920fae7f37b8eb3e2dbecdb0945f45ee8
Boojoo <langer666@web.de>
2013-06-10 17:31:17 +0200

    rule teil3

---
+++ Regelbau/src/de/fuberlin/whitespace/regelbau/logic/actions/TankCapacityState.java
@@ -0,0 +1,13 @@
+package de.fuberlin.whitespace.regelbau.logic.actions;
+
+public class TankCapacityState {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Regelbau/src/de/fuberlin/whitespace/regelbau/logic/triggers/ShowMessage.java
@@ -0,0 +1,13 @@
+package de.fuberlin.whitespace.regelbau.logic.triggers;
+
+public class ShowMessage {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
