485e8185a2fb713b51bddbdec3657507678e52bd
Boojoo <langer666@web.de>
2013-06-10 16:22:09 +0200

    Regel logik

---
+++ Regelbau/src/de/fuberlin/whitespace/regelbau/logic/Action.java
@@ -0,0 +1,14 @@
+package de.fuberlin.whitespace.regelbau.logic;
+
+/**
+ * Stellt eine Schnittstelle für Die Aktionen dar
+ * @author Stefan<
+ *
+ */
+public interface Action {
+
+	/**
+	 * Führt die Action Aus
+	 */
+	public void Do();
+}
