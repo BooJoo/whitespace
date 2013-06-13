da6e7901d68b3264a840bfc06ab9f80212422c36
toastino <christoph.weinhold@googlemail.com>
2013-06-13 22:59:30 +0200

    Scherz geupdatet!

---
+++ Regelbau/src/de/fuberlin/whitespace/ScherzActivity.java
@@ -0,0 +1,25 @@
+package de.fuberlin.whitespace;
+
+import java.util.LinkedList;
+
+import de.fuberlin.whitespace.regelbau.R;
+import de.fuberlin.whitespace.regelbau.logic.Action;
+import de.fuberlin.whitespace.regelbau.logic.Rule;
+import de.fuberlin.whitespace.regelbau.logic.Trigger;
+import de.fuberlin.whitespace.regelbau.logic.actions.SpecialSprintMessage;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridView;
+
+public class ScherzActivity extends Activity {
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+     	setContentView(R.layout.knightrider);
+     	
+	
+    }
+
+}
